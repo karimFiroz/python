ec1def9d67bb1144d6161cd9641743f1e03b8611
karimFiroz <karim.mdkarim.firoz4@gmail.com>
2025-03-12 08:25:35 +0600

    single and multiline comment use for run

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+"""
+# for loop
 for i in range(0,5):
     print(i)
+
+# print String
+print('I am a student')
+print("I am learning python")
+
+
+# print integer
+print(3+2)
+print((3+2)+5)
+
+
+number=3+2
+new_number=number+5
+print(number)
+print(new_number)
+
+"""
