f2c5a52dedc55fb9bbb7eb9367038354f2ff155e
karimFiroz <karim.mdkarim.firoz4@gmail.com>
2025-03-12 16:16:16 +0600

    One program is added

--- main.py
+++ main.py
@@ -18,4 +18,21 @@ new_number=number+5
 print(number)
 print(new_number)
 
+name="Karim"
+age=63
+print(name)
+print(age)
+print(str(21)+name)
+print(type(name))
+print(type(age))
+
+ratio=2.5
+print(type(ratio))
+
+
+print("What is your name?")
+print("Your name please:")
+name = input()
+print(f"Hi {name},Congratulations!")
+
 """
