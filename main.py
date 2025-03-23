04e44ee1fb1b77a175a6aecf0ce4f5a239e54594
karimFiroz <karim.mdkarim.firoz4@gmail.com>
2025-03-22 23:41:22 -0700

    Logical and Compirasion Operator added

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
-"""
+
 # for loop
-for i in range(0,5):
-    print(i)
+#for i in range(0,5):
+#print(i)
 
 # print String
-print('I am a student')
-print("I am learning python")
+#print('I am a student')
+#print("I am learning python")
 
 
 # print integer
-print(3+2)
-print((3+2)+5)
+#print(3+2)
+#print((3+2)+5)
 
 
-number=3+2
-new_number=number+5
-print(number)
-print(new_number)
+#number=3+2
+#new_number=number+5
+#print(number)
+#print(new_number)
 
-name="Karim"
-age=63
-print(name)
-print(age)
-print(str(21)+name)
-print(type(name))
-print(type(age))
+#name="Karim"
+#age=63
+#print(name)
+#print(age)
+#print(str(21)+name)
+#print(type(name))
+#print(type(age))
 
-ratio=2.5
-print(type(ratio))
+#ratio=2.5
+#print(type(ratio))
 
-print("What is your name?")
-print("Your name please:")
-name = input()
-print(f"Hi {name},Congratulations!")
+#print("What is your name?")
+#print("Your name please:")
+#name = input()
+#print(f"Hi {name},Congratulations!")
 
 # variables
-print("Hello World")
-fruits = "mangos"
-print(fruits)
-fruits = "apples"
-print(fruits)
+#print("Hello World")
+#fruits = "mangos"
+#print(fruits)
+#fruits = "apples"
+#print(fruits)
 
 #Data types
-x=5
-y=3.5
-print(x+y)
-w=True
-z=False
-print(w)
-print(z)
-"""
+#x=5
+#y=3.5
+#print(x+y)
+#w=True
+#z=False
+#print(w)
+#print(z)
+
 #Arithmetic Operators
 #print(10 - 7) #Substraction
 #print(10 + 7)#Addition
@@ -69,3 +69,18 @@ print(z)
 #print(y/x)
 #print(x/y)
 #print(x//y)
+#x+=y #ie,x=x+y
+#print(x)
+
+#Comparision Operator true and false
+#x=10
+#y=20
+#print(x!=y)#unequality
+#print(x==y)#Equality
+#compare=x>y#check x>y
+
+#print(compare)
+#compare=x<y #check x<y
+#print(compare)
+
+#Logical Operator
